8b0d0f500d02d9a4bfd74d271d996ef68a3be504
Esteban Garcia <esgarciap@unal.edu.co>
2026-05-27 17:22:22 -0500

    se añadio los programa de la entraga 3

---
+++ Ejercicio de seguimiento 3/Ejercicio_8_2/Ejercicio_8_2_java/src/main/java/com/mycompany/ejercicio_8_2_java/Ejercicio_8_2_java.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio_8_2_java;
+
+/**
+ *
+ * @author esgar
+ */
+public class Ejercicio_8_2_java {
+
+    public static void main(String[] args) {
+        VentanaPrincipal miVentanaPrincipal=new VentanaPrincipal(); /* Define la ventana  principal */
+        miVentanaPrincipal.setVisible(true); /* Establece la ventana como visible */
+            }
+}
---
+++ Ejercicio de seguimiento 3/Ejercicio_8_3/Ejercicio_8_3_java/src/main/java/com/mycompany/ejercicio_8_3_java/Ejercicio_8_3_java.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio_8_3_java;
+
+/**
+ *
+ * @author esgar
+ */
+public class Ejercicio_8_3_java {
+
+    public static void main(String[] args) {
+        VentanaPrincipal miVentanaPrincipal; /* Define la ventana principal */
+        miVentanaPrincipal= new VentanaPrincipal(); /* Crea la ventana principal */
+        miVentanaPrincipal.setVisible(true); /* Establece la ventana como visible */
+        // Establece que la ventana no puede cambiar su tamaño
+        miVentanaPrincipal.setResizable(false);
+            }
+}
