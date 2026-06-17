9e5e14f8871f643ed93d5c869b4fe31666f285c4
Esteban Garcia <esgarciap@unal.edu.co>
2026-06-16 22:16:48 -0500

    Ejercicios 400 terminado entrega 4

---
+++ Ejercicio de seguimiento 4/Ejercicio pagina 400/Ejercicio_400_Java/src/main/java/com/mycompany/ejercicio_400_java/Ejercicio_400_Java.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio_400_java;
+
+/**
+ *
+ * @author esgar
+ */
+public class Ejercicio_400_Java {
+
+    public static void main(String[] args) {
+        VentanaPrincipal miVentanaPrincipal = new VentanaPrincipal();
+        miVentanaPrincipal.setVisible(true);
+        miVentanaPrincipal.setResizable(true);
+        
+  
+    }
+}
